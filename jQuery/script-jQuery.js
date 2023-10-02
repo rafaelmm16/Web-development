737a9324e290d0d379fbbddf69278337e270b77d
rafaelmm16 <rafael.merlomendes@gmail.com>
2023-10-02 17:35:58 -0300

    Estudo sobre jQuery

--- jQuery/script-jQuery.js
+++ jQuery/script-jQuery.js
@@ -1,6 +1,48 @@
 $(function(){
     $('button').click(function() {
-        //$('h1').hide();
-        $('h1').css();
+        //esconde texto
+        //$('h1').hide(); 
+
+        //muda cor
+        $('h1').css("color", "green");
+
+        //muda cor de uma id
+        $('#unic').css("color", "blue");
+    });
+
+    $('#green').click(function() {
+        //muda cor fundo
+        $('p').css("background-color", "green");
+        $('p').fadeOut('slow'); //ou fast
+        $('p').delay(2000);
+        $('p').fadeIn('slow');
+    });
+
+    $('#purple').click(function() {
+        //muda cor
+        $('p')
+        .css("color", "purple")
+        .fadeOut().delay(2000)
+        .fadeIn(5000)
+        .addClass('blue');
+    });
+
+    //SlideShow
+    $('#l1').click(function(){
+        $('#1').show();
+        $('#2').hide();
+        $('#3').hide();
+    });
+
+    $('#l2').click(function(){
+        $('#1').hide();
+        $('#2').show();
+        $('#3').hide();
+    });
+
+    $('#l3').click(function(){
+        $('#1').hide();
+        $('#2').hide();
+        $('#3').show();
     });
 });
