c5b1ea92dc2c15d2fa2fad3ebfe506ec8792af14
rafaelmm16 <rafael.merlomendes@gmail.com>
2023-10-02 14:10:49 -0300

    Estudos sobre JS

---
+++ Form validation/script.js
@@ -0,0 +1,10 @@
+function valida(){
+	var nome = document.getElementById('nome');
+
+	if (nome.value == ""){
+		alert("Campo nome não pode estar em branco.");
+	}
+	else {
+		alert("Nenhum problema foi detectado. Seu formulário pode ser enviado com sucesso.");
+	}
+}
---
+++ jQuery/script-jQuery.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('button').click(function() {
+        //$('h1').hide();
+        $('h1').css();
+    });
+});
