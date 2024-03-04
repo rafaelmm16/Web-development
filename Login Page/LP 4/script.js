078ced96fac05224e9af2fc4b622fbb3ec642685
rafaelmm16 <rafael.merlomendes@gmail.com>
2024-03-04 20:54:48 -0300

    📱 site de login

---
+++ Login Page/LP 4/script.js
@@ -0,0 +1,5 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aqui você pode adicionar a lógica de autenticação
+    alert('Login realizado com sucesso!');
+});
